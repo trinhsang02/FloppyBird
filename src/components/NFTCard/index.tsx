a90a3e4330b8c5326e646e157f4e4d5ecc26e168
trinhsang02 <22521243@gm.uit.edu.vn>
2024-10-22 14:26:01 +0700

    Update: NFT Card

--- src/components/NFTCard/index.tsx
+++ src/components/NFTCard/index.tsx
@@ -1,40 +1,16 @@
 import {
   Dimensions,
   View,
-  StyleSheet,
   Image,
   ActivityIndicator,
   Text,
-  Button,
   TouchableOpacity,
 } from "react-native";
-import {
-  useAccount,
-  useContractRead,
-  useContractWrite,
-  usePrepareContractWrite,
-} from "wagmi";
-import {
-  getBirdAbi,
-  getBirdMarketPlaceAbi,
-  getFloppyAbi,
-} from "../../contracts/utils/getAbis";
 import { NftProps } from "../../type";
-import React from "react";
-import RequestModal from "../RequestModal";
-import { config } from "dotenv";
-import {
-  getBirdAddress,
-  getBirdMarketPlaceAddress,
-  getFloppyAddress,
-} from "../../contracts/utils/getAddress";
-import { useStateContext } from "../../context";
-import { useDispatch } from "react-redux";
-import { bindActionCreators } from "redux";
-import { actionCreators, State } from "../../redux/index";
-import { useSelector } from "react-redux";
-import NFTCElement from "../NFTCard_Element/NFTC_Element";
-const width = Dimensions.get("window").width;
+import React, { useState } from "react";
+import { HeartIcon as HeartIconOutline, ChevronDoubleRightIcon } from "react-native-heroicons/outline";
+import { HeartIcon } from "react-native-heroicons/solid";
+import styles from "./styles";
 
 const NFTCard: React.FC<NftProps> = ({
   nft,
@@ -49,160 +25,89 @@ const NFTCard: React.FC<NftProps> = ({
   const isShowDefaultImageRedBird = id?.toString() === "2";
   const isShowIPFSimage = id?.toString() === "0";
 
-  return (
-    <TouchableOpacity onPress={onPress}>
-      <View style={styles.nft}>
-        {!isLoading ? (
-          <>
-            <View style={styles.NFTCardContainer}>
-        
-              <View style={styles.imageContainer}>
-                {isShowIPFSimage && (
-                  <Image
-                    source={require("../../assets/images/yellowbird-midflap.png")}
-                    style={[styles.nftImage, { aspectRatio: 1 }]}
-                    resizeMode="contain"
-                  />
-                )}
-                {isShowDefaultImageBlueBird && (
-                  <Image
-                    source={require("../../assets/images/bluebird-midflap.png")}
-                    style={[styles.nftImage, { aspectRatio: 1 }]}
-                    resizeMode="contain"
-                  />
-                )}
-                {isShowDefaultImageRedBird && (
-                  <Image
-                    source={require("../../assets/images/redbird-midflap.png")}
-                    style={[styles.nftImage, { aspectRatio: 1 }]}
-                    resizeMode="contain"
-                  />
-                )}
-              </View>
-              <View style={styles.nftcElementContainer}>
-                <NFTCElement />
-              </View>
+  const [isLiked, setIsLiked] = useState(false);
 
-              <View style={styles.textContainer}>
-                {/* NFTcard name */}
-                <Text style={styles.NFTName}>
-                  The Flappy Bird NFT #{(id as any)?.toString()}
-                </Text>
-                {isTransfer && (
-                  <View style={styles.nftprice}>
-                     <View style={{
-                        flexDirection: "row",
-                        alignItems: "center",
-                        }}>
-                       <Text
-                            style={styles.text}
-                            numberOfLines={1}
-                            ellipsizeMode="tail"
-                            >
-                              {parseFloat((price as any)?.toString()) / 1e18} {" "}
-                            </Text>
-                          <Image
-                            source={require('../../../src/assets/images/medal_gold.png')}
-                            style={styles.iconCoin}
-                          />
-                    </View>                   
-                  </View>
-                )}
-              </View>
+  const handleLikePress = (event) => {
+    event.stopPropagation(); // Prevent navigation when click like button
+    setIsLiked(!isLiked); 
+  };
+
+  return (
+    <View style={styles.container}>
+      {/* NFT CARD */}
+      <TouchableOpacity onPress={onPress} style={styles.nft}>
+        {!isLoading ? (
+          <View style={styles.NFTCardContainer}>
+            <View style={styles.imageContainer}>
+              {isShowIPFSimage && (
+                <Image
+                  source={require("../../assets/images/yellowbird-midflap.png")}
+                  style={[styles.nftImage, { aspectRatio: 1 }]}
+                  resizeMode="contain"
+                />
+              )}
+              {isShowDefaultImageBlueBird && (
+                <Image
+                  source={require("../../assets/images/bluebird-midflap.png")}
+                  style={[styles.nftImage, { aspectRatio: 1 }]}
+                  resizeMode="contain"
+                />
+              )}
+              {isShowDefaultImageRedBird && (
+                <Image
+                  source={require("../../assets/images/redbird-midflap.png")}
+                  style={[styles.nftImage, { aspectRatio: 1 }]}
+                  resizeMode="contain"
+                />
+              )}
             </View>
-          </>
+
+            <View style={styles.textContainer}>
+              <Text style={styles.NFTName}>
+                The Flappy Bird NFT #{(id as any)?.toString()}
+              </Text>
+
+              {isTransfer ? (
+                <View style={styles.nftprice}>
+                  <View style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                  }}>
+                    <Text
+                      style={styles.text}
+                      numberOfLines={1}
+                      ellipsizeMode="tail"
+                    >
+                      {parseFloat((price as any)?.toString()) / 1e18} {" "}
+                    </Text>
+                    <Image
+                      source={require('../../../src/assets/images/medal_gold.png')}
+                      style={styles.iconCoin}
+                    />
+                  </View>
+                </View>
+              ) : (
+                <View style={styles.nftprice}>
+                  <ChevronDoubleRightIcon color="white" size={24} />
+                </View>
+              )}
+            </View>
+          </View>
         ) : (
           <ActivityIndicator size="large" style={styles.loadingIndicator} />
         )}
+      </TouchableOpacity>
 
-        {/* <RequestModal
-            isVisible={requestModalVisible}
-            isLoading={isApproveTokenLoading || isBuyNftLoading || isApproveNftLoading}
-            rpcResponse={isApproveTokenSuccess || isBuyNftSuccess || isApproveNftSuccess ? "Success" : undefined}
-            rpcError={isApproveTokenError || isBuyNftError || isApproveNftError ? 'Something Wrong Happened' : undefined}
-            onClose={() => setRequetsModalVisible(false)}
-        /> */}
-      </View>
-    </TouchableOpacity>
+      {/* Like button */}
+      <TouchableOpacity style={styles.likeButton} onPress={handleLikePress}>
+        {isLiked ? (
+          <HeartIcon color="red" size={24} />
+        ) : (
+          <HeartIconOutline color="orange" size={24} />
+        )}
+      </TouchableOpacity>
+    </View>
   );
 };
-
-const styles = StyleSheet.create({
-  nft: {
-    // backgroundColor: "#eee",
-    width: width * 0.75,
-    marginHorizontal: 10,
-    borderRadius: 8,
-    justifyContent: "center",
-  },
-  NFTCardContainer: {
-    flexDirection: "row", // Arrange items horizontally
-  },
-  NFTName: {
-    color: "Black",
-    fontWeight: "bold",
-    textAlign: "auto",
-    fontSize: 16,
-    marginBottom: 10,
-  },
-  textContainer: {
-    backgroundColor: "#eee",
-    flex: 1,
-    marginTop: 8,
-    marginBottom:8,
-    padding: 8,
-    borderWidth: 1,
-    borderColor: "#ddd",
-    borderRadius: 8,
-    justifyContent: "center",
-    borderTopLeftRadius: 0,
-    borderBottomLeftRadius: 0,
-  },
-  nftprice: {
-    backgroundColor: "#612FB1",
-    borderRadius: 30,
-    width: width * 0.3,
-    maxWidth: 200,
-    alignSelf: "center",
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  nftImage: {
-    width: "100%",
-    height: 100,
-    borderRadius: 6,
-  },
-  loadingIndicator: {
-    alignSelf: "center",
-    justifyContent: "center",
-    alignItems: "center",
-    color: "#11C0CB", // cyan color,
-  },
-  text: {
-    color: "white",
-    textAlign: "center",
-    fontSize: 16,
-    fontWeight: "300",
-    marginLeft: 10,
-  },
-  imageContainer: {
-    position: "absolute",
-    marginLeft: 10,
-    marginTop:10,
-    zIndex: 1, 
-    alignItems: "flex-start",
-  },
-  nftcElementContainer: {
-    zIndex: 0, 
-  },
-  iconCoin :{
-    width: 24,
-    height: 24,
-    marginLeft: 5,
-  },
-});
 
 export default NFTCard;
---
+++ src/components/NFTCard/styles.ts
@@ -0,0 +1,95 @@
+import { StyleSheet, Dimensions } from 'react-native';
+const width = Dimensions.get("window").width;
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#EEB067",
+        borderRadius: 43,
+        justifyContent: "space-between",   
+    },
+    likeButton: {
+        position: 'absolute',
+        top: 15, 
+        right: 15, 
+        backgroundColor: 'white',
+        borderRadius: 50, 
+        padding: 10, 
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 2,
+        elevation: 2,
+      },
+    nft: {
+      width: width * 0.8,
+      height: width * 0.8,
+      marginHorizontal: 10,  
+    },
+    NFTCardContainer: {
+      flexDirection: "column", // Arrange items horizontally
+    },
+    textContainer: {
+        backgroundColor: 'rgba(50, 32, 10, 0.26)',
+        alignItems: "center", 
+        padding: 10,
+        marginTop: 50, 
+        flexDirection: "row",
+        borderRadius: 20,
+        justifyContent: "space-between",
+    },
+    nftprice: {
+      backgroundColor: "#0B1E5B",
+      borderRadius: 15,
+      width: width * 0.3,
+      maxWidth: 200,
+      padding: 10,
+      alignSelf: "center",
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    NFTName: {
+        color: "Black",
+        fontWeight: "bold",
+        textAlign: "auto",
+        fontSize: 16,
+        marginBottom: 10,
+        flexWrap: 'wrap', 
+        width: '50%', 
+      },
+    nftImage: {
+      width: "100%",
+      height: 100,
+      borderRadius: 10,
+      marginTop: width*0.2,     
+    },
+    loadingIndicator: {
+      alignSelf: "center",
+      justifyContent: "center",
+      alignItems: "center",
+      color: "#11C0CB", // cyan color,
+    },
+    text: {
+      color: "white",
+      textAlign: "center",
+      fontSize: 16,
+      fontWeight: "500",
+      marginLeft: 10,
+    },
+    imageContainer: {
+      zIndex: 1, 
+      alignItems: "center", 
+      justifyContent: "center", 
+      borderRadius: 10,
+
+    },
+    iconCoin :{
+      width: 24,
+      height: 24,
+      marginLeft: 5,
+    },
+  });
+
+  export default styles;
